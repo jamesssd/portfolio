654f2be5d23b5137b5d81c44c2303fd4ae296b0a
James Dizon <dizonjerlin@gmail.com>
2020-11-24 14:26:19 -0800

    updated issues

--- js/index.js
+++ js/index.js
@@ -11,12 +11,12 @@ navLinks.forEach(link => {
     })
 })
 
-var img = document.getElementsByTagName("img");
-var text = document.getElementById("text");
+// var img = document.getElementsByTagName("href");
+// var text = document.getElementById("text");
 
-for (var i = 0; i < img.length; i++){
-    img[i].addEventListener("mouseover", function(){
-        var alt = this.alt;
-        text.textContent = alt;
-    });
-}
+// for (var i = 0; i < img.length; i++){
+//     href[i].addEventListener("mouseover", function(){
+//         var alt = this.alt;
+//         text.textContent = alt;
+//     });
+// }
