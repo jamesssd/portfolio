6fc4341d8e39c4d4ee979dd69b7bcd207670527a
James Dizon <dizonjerlin@gmail.com>
2020-11-24 14:15:48 -0800

    update profile

--- js/index.js
+++ js/index.js
@@ -10,3 +10,13 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+var img = document.getElementsByTagName("img");
+var text = document.getElementById("text");
+
+for (var i = 0; i < img.length; i++){
+    img[i].addEventListener("mouseover", function(){
+        var alt = this.alt;
+        text.textContent = alt;
+    });
+}
