070a2b85df80f0b455a4a495f02117b4a620901d
James Dizon <dizonjerlin@gmail.com>
2020-12-02 15:01:25 -0800

    added video

--- js/index.js
+++ js/index.js
@@ -10,3 +10,21 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+// Scripting for Modal
+$(document).ready(function(){
+    /* Get iframe src attribute value i.e. YouTube video url
+    and store it in a variable */
+    var url = $("#pitchVideo").attr('src');
+    
+    /* Assign empty url value to the iframe src attribute when
+    modal hide, which stop the video playing */
+    $("#myModal").on('hide.bs.modal', function(){
+        $("#pitchVideo").attr('src', '');
+    });
+    
+    /* Assign the initially stored url back to the iframe src
+    attribute when modal is displayed again */
+    $("#myModal").on('show.bs.modal', function(){
+        $("#pitchVideo").attr('src', url);
+    });
+});
